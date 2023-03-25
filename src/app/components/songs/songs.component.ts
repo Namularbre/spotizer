d88260dba9306c7da4fc0d327b357a566dfac895
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-25 16:37:46 +0100

    remove the filter, I will make it later

--- src/app/components/songs/songs.component.ts
+++ src/app/components/songs/songs.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { SongService } from 'src/app/services/song.service';
 import { Observable } from 'rxjs';
 import { Song } from 'src/app/models/song';
@@ -9,11 +9,16 @@ import { Song } from 'src/app/models/song';
   styleUrls: ['./songs.component.css']
 })
 export class SongsComponent {
+  searchedMusicTitle$  = '';
   songs$! : Observable<Song[]>;
 
   constructor(private service : SongService) {}
 
   ngOnInit() {
     this.songs$ = this.service.getSongs();
+  }
+
+  filterMusicTitle() {
+    return this.service.getSongsByName(this.searchedMusicTitle$);
   }
 }
