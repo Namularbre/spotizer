e6b4218b6cdf05ecc3ab9ba8e6c7d292c9f706d7
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-26 21:15:04 +0200

    added a route to the artist list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtistsComponent } from './components/artists/artists.component';
 import { SongsComponent } from './components/songs/songs.component';
 
 const routes: Routes = [
@@ -7,6 +8,10 @@ const routes: Routes = [
     path : "songs",
     component : SongsComponent
     //children //cf TD3 ! Pas fini, mais ce genre de route est testée et approuvée !
+  },
+  {
+    path : "artists",
+    component : ArtistsComponent
   }
 ];
 
