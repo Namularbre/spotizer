102e24e93e56372860dbec3d5a5c0a0c1e980f4c
Namularbre (Thomas Sazerat) <81816911+Namularbre@users.noreply.github.com>
2023-03-27 21:18:41 +0200

    added a working display for the artist's album

--- src/app/models/album.ts
+++ src/app/models/album.ts
@@ -3,5 +3,29 @@ import { Artist } from "./artist";
 import { Song } from "./song";
 
 export class Album {
-    constructor(private readonly id : number, private title : string, private songs : Song[], private albumType : AlbumType, private artist : Artist, private image : string) {}
+    constructor(private readonly _id : number, private _title : string, private _songs : Song[], private _albumType : AlbumType, private _artist : Artist, private _image : string) {}
+
+    public get id() : number {
+        return this._id;
+    }
+
+    public get title() : string {
+        return this._title;
+    }
+
+    public get songs() : Song[] {
+        return this._songs;
+    }
+
+    public get albumType() : AlbumType {
+        return this._albumType;
+    }
+
+    public get artist() : Artist {
+        return this._artist;
+    }
+
+    public get image() : string {
+        return this._image;
+    }
 }
